34d6dfaa8f92d5119e168ea2893a3fd513cc9fc2
Yagufrix <antonio.sein@g-tec.io>
2018-10-10 12:38:33 -0500

    Movement added
    In the player Script

--- BlackHole/Assets/Scripts/Player.cs
+++ BlackHole/Assets/Scripts/Player.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 	
+	float angle = 0;
+	int xSpeed = 2;
+
 	enum PlayerState{
 		Standing, Jumping, Falling
 	}
 
 	PlayerState currentState = PlayerState.Falling;
+
+
 
 	Rigidbody2D rb;
 
@@ -19,15 +24,18 @@ public class Player : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		GetInput();
+		MovePlayer();
 	}
 
-	void GetInput(){
+
+	//Jump function, when the player is standing, can jump
+ 	void GetInput(){
 		if(Input.GetMouseButton(0)){
 			if(currentState == PlayerState.Standing)
 			{
@@ -37,14 +45,27 @@ public class Player : MonoBehaviour {
 		}
 	}
 
+	//Jump Function
 	void Jump()
 	{
 		currentState = PlayerState.Jumping;
 		rb.velocity = new Vector2(0,5);
 	}
 
+	//Collision with the floor
 	void OnCollisionEnter2D(Collision2D other)
 	{
 		currentState = PlayerState.Standing;
 	}
+
+	//Movement of the player
+	void MovePlayer()
+	{
+		Vector2 pos = transform.position;
+		pos.x = Mathf.Cos(angle)*5;
+		
+		transform.position = pos;
+		angle += Time.deltaTime * xSpeed;
+	}
+
 }
