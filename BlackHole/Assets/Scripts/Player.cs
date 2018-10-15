e1a076afd2020822890cbf05a0ceae0da5febddf
Yagufrix <antonio.sein@g-tec.io>
2018-10-15 14:14:27 -0500

    Mangetismo
    Circle collider

--- BlackHole/Assets/Scripts/Player.cs
+++ BlackHole/Assets/Scripts/Player.cs
@@ -1,86 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
 
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		MovePlayer();
+	}
 
-    float timeCounter = 0;
-
-    public float radius;
-    public float speed;
-    bool isJump = false;
-    float x;
-    float y;
-
-
-    enum PlayerState
-    {
-        Standing, Jumping
-    }
-
-    PlayerState currentState;
-
-
-
-    Rigidbody2D rb;
-
-    void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        radius = 2;
-        speed = 1;
-    }
-
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Oscillator();
-
-    }
-
-
-
-    //Jump Function
-    public void Jump()
-    {
-        if (isJump == false)
-        {
-            isJump = true;
-            radius = 1;
-            x = Mathf.Cos(timeCounter) * radius;
-            y = Mathf.Sin(timeCounter) * radius;
-            transform.position = new Vector2(x, y);
-
-			
-
-        }else if (isJump == true)
-		{
-			isJump = false;
-			radius = 2;
-			x = Mathf.Cos(timeCounter) * radius;
-			y = Mathf.Sin(timeCounter) * radius;
-
-			transform.position = new Vector2(x,y);
-		}
-    }
-
-    void Oscillator()
-    {
-        timeCounter += Time.deltaTime * speed;
-
-        x = Mathf.Cos(timeCounter) * radius;
-        y = Mathf.Sin(timeCounter) * radius;
-
-        transform.position = new Vector2(x, y);
-    }
-
+	void MovePlayer()
+	{
+		//this.transform.Rotate(0,0,power);
+		//this.GetComponent<Rigidbody2D>().AddForce(transform.up * boostSpeed,0);
+		//transform.Translate(Vector3.forward * (speed * Time.deltaTime));
+		//Rigidbody2D.AddForce(transform.up * boostSpeed * 10);
+		
+		
+	}
 }
---
+++ Blackhole/Assets/Scripts/NoGrav.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoGrav : MonoBehaviour {
+
+	private Rigidbody2D rig;
+
+	// Use this for initialization
+	void Start () {
+		rig = this.GetComponent<Rigidbody2D>();
+		rig.gravityScale = 1f;
+		StartCoroutine("gravSet");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator gravSet()
+	{
+		yield return new WaitForSeconds(1);
+		rig.gravityScale = 0f;
+	}	
+}
