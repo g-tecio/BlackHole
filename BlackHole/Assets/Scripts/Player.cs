41ad28bad41783a7ad34c72ff2e28b4f74e2e9c6
Yagufrix <antonio.sein@g-tec.io>
2018-10-15 16:50:16 -0500

    Intento de jump
    agregar jump

--- BlackHole/Assets/Scripts/Player.cs
+++ BlackHole/Assets/Scripts/Player.cs
@@ -3,27 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+	public float jumpForce;
+	public int moveSpeed = 0;
+	private Rigidbody2D rb;
+	private float moveInput;
+	public Transform feetPos;
+	public float checkRadius;
+	public LayerMask whatIsGround;
+	
+	
 
-	
-	
+
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody2D>();
 	}
+
 	
 	// Update is called once per frame
-	void Update () 
+	void FixedUpdate () 
 	{
 		MovePlayer();
+		
+		
 	}
+
+	void Update()
+	{
+		isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+		if(isGrounded == true && Input.GetKeyDown(KeyCode.Space)){
+			rb.velocity = Vector2.up * jumpForce;
+		}
+	}
+	
 
 	void MovePlayer()
 	{
-		//this.transform.Rotate(0,0,power);
-		//this.GetComponent<Rigidbody2D>().AddForce(transform.up * boostSpeed,0);
-		//transform.Translate(Vector3.forward * (speed * Time.deltaTime));
-		//Rigidbody2D.AddForce(transform.up * boostSpeed * 10);
+		this.transform.position += transform.right * moveSpeed * Time.deltaTime;
 		
 		
 	}
+
+
+
 }
